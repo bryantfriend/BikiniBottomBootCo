bb325a7566288d61f8f0990174bac31ce15a0245
bryantfriend <72289778+bryantfriend@users.noreply.github.com>
2025-03-04 09:03:42 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+let cart = [];
 
+function addToCart(model) {
+    if (cart.length < 3) {
+        cart.push(model);
+        alert("Added to cart: " + model);
+    } else {
+        alert("Cart is full! Max 3 items.");
+    }
+}
+
+function viewCart() {
+    if (cart.length === 0) {
+        alert("Your cart is empty.");
+    } else {
+        let message = "Your cart:\n";
+        for (let i = 0; i < cart.length; i++) {
+            message += cart[i] + "\n";
+        }
+        alert(message);
+    }
+}
+
+function calculateTotal() {
+    let total = cart.length * 50; // $50 per boot
+    alert("Total price: $" + total);
+}
+
+function clearCart() {
+    cart = [];
+    alert("Cart cleared!");
+}
